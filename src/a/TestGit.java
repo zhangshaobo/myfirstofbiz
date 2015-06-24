299c753b9af896d8a068c7ad1cbdf2d77efe9a7b
zhangshaobo <zhangshaobo1989@126.com>
2015-06-24 12:27:45 +0800

    alter

--- src/a/TestGit.java
+++ src/a/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("this is zsb's git ");
+ System.out.println("this is zsb's  git "); 
 	}
 
 }
